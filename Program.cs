6318c613fa5419b9f94c315da08c2589cdb007cf
LuxuryAgency <84640982+LuxuryAgency@users.noreply.github.com>
2024-10-17 15:15:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace autohaladas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            auto a = new auto { Fogyasztas = 7, MaxÜzemanyag = 55, MegTettUt = 0,Üzemagyagszint=55 };
+  
+            a.fele += Fele;
+            a.kifogy += Kifogy;
+            a.Haladas(500);
+            a.Haladas(500);
+            a.Haladas(500);
+            a.Haladas(500);
+
+        }
+
+        static void Fele(int megtett)
+        {
+            Console.WriteLine("Fele üa elfogyott");
+            Console.WriteLine($"Megtett ut:{megtett}");
+        }
+        static void Kifogy(int megtett)
+        {
+            Console.WriteLine("üa elfogyott");
+            Console.WriteLine($"Megtett ut:{megtett}");
+        }
+    }
+}
---
+++ auto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autohaladas
+{
+    public delegate void Halad(int megtett);
+    public class auto
+    {
+        
+        public event Halad fele;
+        public event Halad kifogy;
+
+        public double Üzemagyagszint {  get; set; }
+        public int MaxÜzemanyag {get; set; }
+
+        public int MegTettUt {  get; set; }
+
+        public double Fogyasztas {  get; set; }
+
+        public bool jelzes = false;
+
+        public void Haladas(int km)
+        {
+            MegTettUt += km;
+            Üzemagyagszint -= (km/100)*Fogyasztas;
+
+            if (Üzemagyagszint <= MaxÜzemanyag / 2)
+            {
+
+                fele?.Invoke(MegTettUt);
+                jelzes = true;
+
+            }
+            if (Üzemagyagszint == 0)
+            {
+                kifogy?.Invoke(MegTettUt);
+                jelzes = true;
+            }
+            
+
+        }
+    }
+}
